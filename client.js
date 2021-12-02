d156243e9a774ba9d83156192a4ef44f78fe7088
Kevin Tan Yulo <yulokevin@gmail.com>
2021-12-01 16:11:22 -0800

    name shows

--- client.js
+++ client.js
@@ -1,11 +1,21 @@
+const net = require('net');
 const connect = function () {
   const conn = net.createConnection({
-    host: // IP address here,
-    port: // PORT number here,
+    host: "165.227.47.243",
+    port: 50541
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on("connect", (data) => {
+    console.log("Successfully connected.")
+
+    conn.write("Name: KY")
+
+  } )
+
+
 
   return conn;
 };
